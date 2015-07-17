17a3cc2cf464ab52d627f0a1c07aeb3c773cc3e3
MAC-ATL019922 <mac-atl019922@MAC-ATL019922-Workroom.local>
2015-07-17 09:52:08 -0400

    Initial Commit

---
+++ SurveySession/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SurveySession
+//
+//  Created by MAC-ATL019922 on 7/17/15.
+//  Copyright © 2015 nikhowlett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
